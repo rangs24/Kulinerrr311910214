9262734980a394ba2f4db648876a96ef3cdf7d3a
rangs24 <ranggapp14@gmail.com>
2022-07-15 07:51:49 +0700

    Initial commit

---
+++ app/src/main/java/com/rangga/kulinerrr311910214/MainActivity.java
@@ -0,0 +1,14 @@
+package com.rangga.kulinerrr311910214;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
